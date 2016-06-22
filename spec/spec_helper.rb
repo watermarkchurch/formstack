929f56ce4c76e5c223d34f745314be05346ec48a
Travis Petticrew <travis@petticrew.net>
2016-06-22 16:13:32 -0500

    Add configuration for testing

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,10 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'formstack'
+require 'rspec/mocks'
+
+RSpec.configure do |config|
+  config.run_all_when_everything_filtered = true
+  config.filter_run :focus
+
+  config.order = 'random'
+end
