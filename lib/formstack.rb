90d9e3e9f871eabb038f3eb3cf9be4d8c5b2ae02
Travis Petticrew <travis@petticrew.net>
2016-06-23 14:01:55 -0500

    Add a Formstack.client method
    This will be the default client instance that is used in models.

--- lib/formstack.rb
+++ lib/formstack.rb
@@ -1,5 +1,11 @@
 require 'formstack/version'
+require 'formstack/form'
 require 'formstack/client'
 
 module Formstack
+
+  def self.client
+    @client ||= Client.new
+  end
+
 end
--- spec/formstack_spec.rb
+++ spec/formstack_spec.rb
@@ -4,4 +4,14 @@ describe Formstack do
   it 'has a version number' do
     expect(Formstack::VERSION).not_to be nil
   end
+
+  describe "#client" do
+    it "returns an instance of the Client class" do
+      expect(Formstack.client).to be_a(Formstack::Client)
+    end
+
+    it "caches the value" do
+      expect(Formstack.client.object_id).to eq(Formstack.client.object_id)
+    end
+  end
 end
