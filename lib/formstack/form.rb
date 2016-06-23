a3b083d5b2d843f7e039d053482188d1f06c6b1c
Travis Petticrew <travis@petticrew.net>
2016-06-23 14:04:00 -0500

    Add each of the models mapping to API concepts
    Each of these models includes the Model module and mostly just the
    methods required for configuration. The Form class has a little bit of
    extra convenience since it is the core model in the schema. These models
    now allow for things like `Form.all` and `Form.create(name: "Foo")`.
    Mostly just a nicer API than the raw Client.

---
+++ lib/formstack/form.rb
@@ -0,0 +1,99 @@
+require 'formstack/model'
+
+module Formstack
+  class Form
+    include Model
+
+    def copy
+      new_from_response client.copy_form self[:id]
+    end
+
+    def fields
+      @fields ||= if self[:fields]
+                    self[:fields].map { |f| Field.new(f) }
+                  else
+                    Field.all(self[:id])
+                  end
+    end
+
+    def submissions
+      @submissions ||= Submission.all(self[:id])
+    end
+
+    def confirmation_emails
+      @confirmation_emails ||= ConfirmationEmail.all(self[:id])
+    end
+
+    def notification_emails
+      @notification_emails ||= NotificationEmail.all(self[:id])
+    end
+
+    def webhooks
+      @webhooks ||= Webhook.all(self[:id])
+    end
+
+    def self.client_method
+      :form
+    end
+
+    def self.response_nesting
+      "forms"
+    end
+  end
+
+  class Field
+    include Model
+
+    def self.client_method
+      :field
+    end
+  end
+
+  class Submission
+    include Model
+
+    def self.client_method
+      :submission
+    end
+
+    def self.response_nesting
+      "submissions"
+    end
+  end
+
+  class ConfirmationEmail
+    include Model
+
+    def self.client_method
+      :confirmation_email
+    end
+
+    def self.response_nesting
+      "confirmations"
+    end
+  end
+
+  class NotificationEmail
+    include Model
+
+    def self.client_method
+      :notification_email
+    end
+
+    def self.response_nesting
+      "notifications"
+    end
+  end
+
+  class Webhook
+    include Model
+
+    def self.client_method
+      :webhook
+    end
+
+    def self.response_nesting
+      "webhooks"
+    end
+  end
+end
---
+++ spec/formstack/form_spec.rb
@@ -0,0 +1,163 @@
+require 'spec_helper'
+
+RSpec.describe Formstack::Form do
+  let(:client) { instance_spy(Formstack::Client) }
+
+  before :each do
+    allow(Formstack).to receive(:client).and_return(client)
+  end
+
+  it "includes Model module" do
+    expect(described_class.included_modules).to include(Formstack::Model)
+  end
+
+  it "is configured" do
+    expect(described_class.client_method).to eq(:form)
+    expect(described_class.response_nesting).to eq("forms")
+  end
+
+  describe "#copy" do
+    it "calls #copy_form on client and returns a new Form object with result" do
+      object = described_class.new("id" => "123")
+      expect(client).to receive(:copy_form).with("123").and_return("id" => "456")
+      new_object = object.copy
+      expect(new_object[:id]).to eq("456")
+    end
+  end
+
+  describe "#fields" do
+    it "initializes from attributes if fields key present" do
+      object = described_class.new("fields" => ["id" => "123"])
+      fields = object.fields
+      expect(fields.size).to eq(1)
+      expect(fields.first["id"]).to eq("123")
+    end
+
+    it "delegates to Field.all with id attribute if fields key not present" do
+      object = described_class.new("id" => "123")
+      allow(client).to receive(:fields).with("123").and_return(["id" => "456"])
+      fields = object.fields
+      expect(fields.size).to eq(1)
+      expect(fields.first["id"]).to eq("456")
+    end
+
+    it "caches the value" do
+      object = described_class.new("fields" => ["id" => "123"])
+      fields = object.fields
+      expect(fields.object_id).to eq(object.fields.object_id)
+    end
+  end
+
+  describe "#submissions" do
+    it "delegates to Submission.all with id attribute" do
+      object = described_class.new("id" => "123")
+      expect(Formstack::Submission).to receive(:all).with("123").and_return(:value)
+      expect(object.submissions).to eq(:value)
+    end
+
+    it "caches the value" do
+      object = described_class.new
+      allow(Formstack::Submission).to receive(:all) { Array.new }
+      expect(object.submissions.object_id).to eq(object.submissions.object_id)
+    end
+  end
+
+  describe "#confirmation_emails" do
+    it "delegates to ConfirmationEmail.all with id attribute" do
+      object = described_class.new("id" => "123")
+      expect(Formstack::ConfirmationEmail).to receive(:all).with("123").and_return(:value)
+      expect(object.confirmation_emails).to eq(:value)
+    end
+
+    it "caches the value" do
+      object = described_class.new
+      allow(Formstack::ConfirmationEmail).to receive(:all) { Array.new }
+      expect(object.confirmation_emails.object_id).to eq(object.confirmation_emails.object_id)
+    end
+  end
+
+  describe "#notification_emails" do
+    it "delegates to NotificationEmail.all with id attribute" do
+      object = described_class.new("id" => "123")
+      expect(Formstack::NotificationEmail).to receive(:all).with("123").and_return(:value)
+      expect(object.notification_emails).to eq(:value)
+    end
+
+    it "caches the value" do
+      object = described_class.new
+      allow(Formstack::NotificationEmail).to receive(:all) { Array.new }
+      expect(object.notification_emails.object_id).to eq(object.notification_emails.object_id)
+    end
+  end
+
+  describe "#webhooks" do
+    it "delegates to Webhook.all with id attribute" do
+      object = described_class.new("id" => "123")
+      expect(Formstack::Webhook).to receive(:all).with("123").and_return(:value)
+      expect(object.webhooks).to eq(:value)
+    end
+
+    it "caches the value" do
+      object = described_class.new
+      allow(Formstack::Webhook).to receive(:all) { Array.new }
+      expect(object.webhooks.object_id).to eq(object.webhooks.object_id)
+    end
+  end
+end
+
+
+RSpec.describe Formstack::Field do
+  it "includes Model module" do
+    expect(described_class.included_modules).to include(Formstack::Model)
+  end
+
+  it "is configured" do
+    expect(described_class.client_method).to eq(:field)
+    expect(described_class.response_nesting).to eq("")
+  end
+end
+
+RSpec.describe Formstack::Submission do
+  it "includes Model module" do
+    expect(described_class.included_modules).to include(Formstack::Model)
+  end
+
+  it "is configured" do
+    expect(described_class.client_method).to eq(:submission)
+    expect(described_class.response_nesting).to eq("submissions")
+  end
+end
+
+RSpec.describe Formstack::ConfirmationEmail do
+  it "includes Model module" do
+    expect(described_class.included_modules).to include(Formstack::Model)
+  end
+
+  it "is configured" do
+    expect(described_class.client_method).to eq(:confirmation_email)
+    expect(described_class.response_nesting).to eq("confirmations")
+  end
+end
+
+RSpec.describe Formstack::NotificationEmail do
+  it "includes Model module" do
+    expect(described_class.included_modules).to include(Formstack::Model)
+  end
+
+  it "is configured" do
+    expect(described_class.client_method).to eq(:notification_email)
+    expect(described_class.response_nesting).to eq("notifications")
+  end
+end
+
+RSpec.describe Formstack::Webhook do
+  it "includes Model module" do
+    expect(described_class.included_modules).to include(Formstack::Model)
+  end
+
+  it "is configured" do
+    expect(described_class.client_method).to eq(:webhook)
+    expect(described_class.response_nesting).to eq("webhooks")
+  end
+end
+
